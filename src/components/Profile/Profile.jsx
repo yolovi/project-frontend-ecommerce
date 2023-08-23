689216d15b81e6bc56c7f95f18cd7cfdcae850e0
yolovi <yolandalopevi@gmail.com>
2023-08-23 20:56:57 +0200

    "flex to div profile container and variables name and lastname in card image profile"

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -25,15 +25,15 @@ const GetUserLogged = () => {
   }
 
   return (
-    <div>
+    <div className="container-profile">
       <div className="container">
         <div class="image">
           <img src="https://images.unsplash.com/photo-1492288991661-058aa541ff43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"></img>
         </div>
         <div class="content">
           <div class="info">
-            <h2>Andrew Neil</h2>
-            <span>Web Developer</span>
+            <h2>{user.name_user}</h2>
+            <span>{user.last_name}</span>
           </div>
         </div>
         <div>
@@ -66,57 +66,25 @@ const GetUserLogged = () => {
           <div className="message">General message</div>
           <div className="message-success">Successfully message</div>
         </Space>
-      </div>
 
-      {/* Renderizar los detalles de los pedidos */}
-      {user.Orders.map((order) => (
-        <div key={order.id}>
-          <p>Order ID: {order.id}</p>
-          <p>Date: {order.createdAt}</p>
-          <p>Products:</p>
-          {order.Products.map((product) => (
-            <div key={product.id}>
-              <p>Name: {product.name_product}</p>
-              <p>Price: {product.price}</p>
-            </div>
-          ))}
-          <hr />
-        </div>
-      ))}
+        {/* Renderizar los detalles de los pedidos */}
+        {user.Orders.map((order) => (
+          <div key={order.id}>
+            <p>Order ID: {order.id}</p>
+            <p>Date: {order.createdAt}</p>
+            <p>Products:</p>
+            {order.Products.map((product) => (
+              <div key={product.id}>
+                <p>Name: {product.name_product}</p>
+                <p>Price: {product.price}</p>
+              </div>
+            ))}
+            <hr />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
-
-//   return (
-//     <div>
-//         <div>
-//       <Space direction="vertical" size={16}>
-//         <Card size="small" title={user.name_user} style={{ width: 300 }}>
-//           <p>Apellido: {user.last_name}</p>
-//           <p>Email: {user.email}</p>
-//         </Card>
-//         <div className="message">General message</div>
-//         <div className="message-success">Successfully message</div>
-//       </Space>
-//     </div>
-
-//       {/* Renderizar los detalles de los pedidos */}
-//       {user.Orders.map((order) => (
-//         <div key={order.id}>
-//           <p>Order ID: {order.id}</p>
-//           <p>Date: {order.createdAt}</p>
-//           <p>Products:</p>
-//           {order.Products.map((product) => (
-//             <div key={product.id}>
-//               <p>Name: {product.name_product}</p>
-//               <p>Price: {product.price}</p>
-//             </div>
-//           ))}
-//           <hr />
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
 
 export default GetUserLogged;
